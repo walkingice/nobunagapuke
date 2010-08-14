a143a0a0ec697eb03195d8ced0ecd8e747efa6b1
walkingice0204 <walkingice0204@91e0df3c-5cb4-1a7d-9289-e19fb2fdd6cc>
2010-08-14 17:47:12 +0000

    Add org.zeroxlab.game
    the code comes from example
    
    git-svn-id: https://nobunagapuke.googlecode.com/svn/trunk@5 91e0df3c-5cb4-1a7d-9289-e19fb2fdd6cc

---
+++ src/org/zeroxlab/game/MainActivity.java
@@ -0,0 +1,29 @@
+package org.zeroxlab.game;
+
+import com.stickycoding.rokon.DrawPriority;
+import com.stickycoding.rokon.RokonActivity;
+
+public class MainActivity extends RokonActivity {
+
+	public static final float GAME_WIDTH = 480f;
+	public static final float GAME_HEIGHT = 320f;
+
+	private GameScene mGameScene;
+
+	public void onCreate() {
+		debugMode();
+		forceFullscreen();
+		forceLandscape();
+		setGameSize(GAME_WIDTH, GAME_HEIGHT);
+		setDrawPriority(DrawPriority.PRIORITY_VBO);
+		setGraphicsPath("textures/");
+		createEngine();
+	}
+
+	public void onLoadComplete() {
+		Textures.load();
+                mGameScene = new GameScene();
+		setScene(mGameScene);
+	}
+
+}
